bd47527b3fdcbfad1a4e1fdb2e1c329e74f87766
Faishal Azka <azukineru@gmail.com>
2017-05-15 11:40:39 +0700

    Few mistakes

--- login.php
+++ login.php
@@ -3,12 +3,9 @@
 	$error='';
 	
 	if(isset($_POST['login'])){
-<<<<<<< HEAD
-=======
 		$con=mysqli_connect("localhost", "root", "");
 		mysqli_select_db($con, "ojt");
 
->>>>>>> experiment
 		if(empty($_POST['username'])||empty($_POST['password'])){
 			$error="Username and Password is required to log in";
 		}else{
@@ -17,17 +14,6 @@
 			
 			$username=stripslashes($username);
 			$password=stripslashes($password);
-<<<<<<< HEAD
-			$username=mysql_real_escape_string($username);
-			$password=md5(mysql_real_escape_string($password));
-			
-			$con=mysql_connect("localhost", "root", "");
-			mysql_select_db("ojt");
-			
-			$query=mysql_query("SELECT * FROM account WHERE username='$username' AND password='$password'");
-			
-			if(mysql_num_rows($query)==1){
-=======
 			$username=mysqli_real_escape_string($con, $username);
 			$password=md5(mysqli_real_escape_string($con, $password));
 			
@@ -36,17 +22,12 @@
 			$query=mysqli_query($con, "SELECT * FROM account WHERE username='$username' AND password='$password'");
 			
 			if(mysqli_num_rows($query)==1){
->>>>>>> experiment
 				$_SESSION['login_user']=$username;
 				header("Location:dashboard.html");
 			}else{
 				$error="Username or Password is invalid";
 			}
-<<<<<<< HEAD
-			mysql_close($con);
-=======
 			mysqli_close($con);
->>>>>>> experiment
 		}	
 	}
 ?>
--- session.php
+++ session.php
@@ -1,32 +1,17 @@
 <?php
-<<<<<<< HEAD
-	$con=mysql_connect("localhost", "root", "");
-	mysql_select_db("ojt");
-=======
 	$con=mysqli_connect("localhost", "root", "");
 	mysqli_select_db($con, "ojt");
->>>>>>> experiment
 	
 	session_start();
 	
 	$user_check=$_SESSION['login_user'];
-<<<<<<< HEAD
-	$query=mysql_query("SELECT username FROM account WHERE username='$user_check'");
-	$row=mysql_fetch_assoc($query);
-=======
 	$query=mysqli_query($con, "SELECT username FROM account WHERE username='$user_check'");
 	$row=mysqli_fetch_assoc($query);
->>>>>>> experiment
 	
 	$login_session=$row['username'];
 	
 	if(!isset($login_session)){
-<<<<<<< HEAD
-		mysql_close($con);
-		header('Location:index.php');
-=======
 		mysqli_close($con);
 		//header('Location:index.php');
->>>>>>> experiment
 	}
 ?>
