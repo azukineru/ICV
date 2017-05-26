2f51db043a704d54f2bcf98702dc102c36c32821
doel-mubarok <bdoel.a.bdoel@gmail.com>
2017-05-26 09:35:36 +0700

    Update clusterlist.php

--- clusterlist.php
+++ clusterlist.php
@@ -7,7 +7,20 @@
 		$con=mysqli_connect("localhost", "root", "");
 		mysqli_select_db($con, "ojt");
 			
-		$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."' AND `DATEL/SOC` = '".$SOC."'");
+		if($SOC=="All"){
+			$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."'");
+		}elseif($SOC=="SMT"){
+			$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."' AND 
+			(`DATEL/SOC` = 'BOJ' OR `DATEL/SOC` = 'MKG' OR `DATEL/SOC` = 'SMT' OR `DATEL/SOC` = 'TGU')");
+		}elseif($SOC=="JHR"){
+			$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."' AND 
+			(`DATEL/SOC` = 'GNK' OR `DATEL/SOC` = 'JHR')");
+		}elseif($SOC=="SMC"){
+			$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."' AND 
+			(`DATEL/SOC` = 'SMC' OR `DATEL/SOC` = 'CANDI')");	
+		}else{
+			$query=mysqli_query($con, "SELECT * FROM cluster WHERE 9Cluster = '".$segment."' AND HS = '".$HS."' AND `DATEL/SOC` = '".$SOC."'");
+		}
 					
 		if(mysqli_num_rows($query)>0){
 			while ($row = mysqli_fetch_array($query)){ 
