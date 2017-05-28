d5bedc69a5ee32f85ebd38ac0c5419aff0d724be
doel-mubarok <bdoel.a.bdoel@gmail.com>
2017-05-28 14:56:25 +0700

    Add files via upload

---
+++ custdata.php
@@ -0,0 +1,28 @@
+<?php
+	$con=mysqli_connect("localhost", "root", "");
+	mysqli_select_db($con, "ojt");
+	
+	$no=$_GET['no'];
+	$nquery=mysqli_query($con, "SELECT ODP FROM cluster WHERE No = '".$no."'");
+	
+	$data=mysqli_fetch_array($nquery);
+	$odp=$data['ODP'];
+	
+	header("Content-type: application/vnd-ms-excel");
+	header("Content-Disposition: attachment; filename=customer_".$odp.".xls");
+	
+	$query=mysqli_query($con, "SELECT * FROM customer WHERE RK = '".$odp."'");
+	
+	echo '<table border="1">';
+	echo '<tr>';
+	echo '<td>ND</td>'; 
+	echo '<td>Nama</td>';  
+	echo '</tr>';
+	while ($row = mysqli_fetch_array($query)){ 
+		echo '<tr>';
+		echo '<td>'.$row['ND'].'</td>'; 
+		echo '<td>'.$row['NAMA'].'</td>';   
+		echo '</tr>';  
+	}
+	echo '</table>';
+?>
