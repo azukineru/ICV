7831be44804c2b9693952f95094d76df85f43717
doel-mubarok <bdoel.a.bdoel@gmail.com>
2017-05-31 11:00:12 +0700

    Add files via upload

--- searchclust.php
+++ searchclust.php
@@ -3,8 +3,22 @@
 		$con=mysqli_connect("localhost", "root", "");
 		mysqli_select_db($con, "ojt");
 		if (isset($_REQUEST['search'])) {
-			$nameclust = mysqli_real_escape_string($con, $_REQUEST['nameclust']);     
-			$sql = "SELECT * FROM cluster WHERE `Nama Cluster` LIKE '%".$nameclust."%'"; 
+			$nameclust = mysqli_real_escape_string($con, $_REQUEST['nameclust']);
+
+			$search_exploded=explode(" ",$nameclust);
+			$count=0; 
+			$construct=" ";
+			
+			foreach($search_exploded as $search_each){
+				$count++;
+				if($count==1){
+					$construct.="`Nama Cluster` LIKE '%".$search_each."%'";
+				}else{
+					$construct.=" AND `Nama Cluster` LIKE '%".$search_each."%'";
+				}
+			}
+			
+			$sql = "SELECT * FROM cluster WHERE".$construct; 
 			$query = mysqli_query($con, $sql); 
 
 			echo '<div class="scroll-box">';
