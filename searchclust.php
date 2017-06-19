7c47e6b4462bb9ef0ad85675f4a4081528a92d8e
doel-mubarok <bdoel.a.bdoel@gmail.com>
2017-06-19 10:13:39 +0700

    Add files via upload

--- searchclust.php
+++ searchclust.php
@@ -11,20 +11,20 @@
 			foreach($search_exploded as $search_each){
 				$count++;
 				if($count==1){
-					$construct.="`Nama Cluster` LIKE '%".$search_each."%' OR `ODP` LIKE '%".$search_each."%'";
+					$construct.="`Nama Cluster` LIKE '%".$search_each."%'";
 				}else{
-					$construct.=" AND (`Nama Cluster` LIKE '%".$search_each."%' OR `ODP` LIKE '%".$search_each."%')";
+					$construct.=" AND `Nama Cluster` LIKE '%".$search_each."%'";
 				}
 			}
 			
-			$sql = "SELECT * FROM cluster WHERE".$construct; 
+			$sql = "SELECT DISTINCT `Nama Cluster` FROM cluster WHERE".$construct; 
 			$query = mysqli_query($con, $sql); 
 			echo '<div class="scroll-box">';
                 echo '<table class="table-cluster">';
 					echo '<tbody>';
 						while ($row = mysqli_fetch_array($query)){
 							echo '<tr>';
-								echo '<td><a href="page_view-datacluster.php?segment='.$row['9Cluster'].'&no='.$row['No'].'&search=1">'.$row['ODP'].'-'.$row['Nama Cluster'].'</a></td>';
+								echo '<td><a href="page_view-datacluster.php?segment='.$row['9Cluster'].'&no='.$row['No'].'&search=1">'.$row['Nama Cluster'].'</a></td>';
 							echo '</tr>';
 						}
 					echo '</tbody>';
